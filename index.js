a65b4da4b7ffd3cc76062e00962db60f95bef6ac
Alessandro Giordo <alessandro.giordo@creativevirtual.com>
2016-05-13 11:09:21 +0100

    new app

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var app = require('./app');
 
 chattybot.listen(app,
   process.env.PORT || 5000,
-  "EAAIEfsNb6YkBAOANnLIvadLtfAkrkzByZAdecDwadLsszJOuRvpH4BV70YoSDANOdg3KHZBPYJ9wnNHo9AG5cwmp2rWjJJfSDqrcngxH40VG6QG6ZBFZCD5IHg3uKDLzGKRd1DCo9Xm9EOefAfoifLGL6wJQntCYiykx78zmPwZDZD",
+  "EAAD3hgUX4LYBAJCpQY22efTLmO1c5TxwWbJcEMfw7hNmwWiuPU7JUjW1OYF54umUhwo4ss6czvZBLPX0tPET5Saq3IsSfHrk5779ERSgYpFuhptDJWbKBk7tuRmuNU3qJCRyJ3lUvuRoZCkk4iLPU7tiu1VzLcUZBtK61jw2gZDZD",
   "this_is_my_awesome_token");
