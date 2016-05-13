c57b66aae3768e7e9564ca37719bd0c322e8af07
Alessandro Giordo <alessandro.giordo@creativevirtual.com>
2016-05-13 10:13:47 +0100

    changed token

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var app = require('./app');
 chattybot.listen(app,
   process.env.PORT || 5000,
   EAAIEfsNb6YkBAOANnLIvadLtfAkrkzByZAdecDwadLsszJOuRvpH4BV70YoSDANOdg3KHZBPYJ9wnNHo9AG5cwmp2rWjJJfSDqrcngxH40VG6QG6ZBFZCD5IHg3uKDLzGKRd1DCo9Xm9EOefAfoifLGL6wJQntCYiykx78zmPwZDZD,
-  my_voice_is_my_password_verify_me);
+  this_is_my_awesome_token);
