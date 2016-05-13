365196b304331a511084b12daf2c554a17b9863b
Alessandro Giordo <alessandro.giordo@creativevirtual.com>
2016-05-13 10:26:39 +0100

    added start script

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var chattybot = require('chatty-bot');
 var app = require('./app');
 
 chattybot.listen(app,
-  process.env.PORT || 5000,
+  process.env.PORT || 3000,
   EAAIEfsNb6YkBAOANnLIvadLtfAkrkzByZAdecDwadLsszJOuRvpH4BV70YoSDANOdg3KHZBPYJ9wnNHo9AG5cwmp2rWjJJfSDqrcngxH40VG6QG6ZBFZCD5IHg3uKDLzGKRd1DCo9Xm9EOefAfoifLGL6wJQntCYiykx78zmPwZDZD,
   this_is_my_awesome_token);
